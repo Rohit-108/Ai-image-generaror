858bf56aec70531570c9c713b15aa4657a763946
Rohit-108 <rohitsharma1082001@gmail.com>
2024-07-10 23:52:45 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import ImageGenerator from "./components/ImageGenerator/ImageGenerator";
+
+import './App.css';
+
+function App() {
+  return (
+   <div>
+    <ImageGenerator />
+   </div>
+  );
+}
+
+export default App;
